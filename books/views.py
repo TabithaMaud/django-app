f6b4bef4b16d97234804438aed3b67a10d257013
TabithaMaud <tabitha.m.perry@gamil.com>
2021-02-26 10:36:56 -0500

    build out more

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Book
 
 # Create your views here.
+
+
+def book_list(request):
+    data = Book.objects.all()
+    data = list(data.values())
+    return JsonResponse(data, safe=False)
